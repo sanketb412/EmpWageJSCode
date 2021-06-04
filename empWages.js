41e526ef7a7ebb457cc0928ff6a422e1790e8301
sanketb412 <sanketbagde38@gmail.com>
2021-06-04 15:07:29 +0530

    Added: code for Calculating Employee Daily Wage Using Switch case

--- empWages.js
+++ empWages.js
@@ -1,9 +1,36 @@
-console.log("<<###...Welcome to EmpWage Program...###>>")
-
-const IS_ABSENT = 0
-let empCheck = Math.floor(Math.random() * 10) % 2;
-if (empCheck == IS_ABSENT){
-    console.log("Employee is Absent");
-} else {
-    console.log("Employee is Present");
+/**
+ * UC 1 Checking Employee Present or Absent
+*/
+{
+    const IS_ABSENT = 0
+    let empCheck = Math.floor(Math.random() * 10) % 2;
+    if (empCheck == IS_ABSENT){
+        console.log("UC1- Employee is Present.");
+    } else {
+        console.log("UC1- Employee is Absent.");
+    }
 }
+const IS_PART_TIME = 1;
+const IS_FULL_TIME = 2;
+const FULL_TIME_HOURS = 4;
+const PART_TIME_HOURS = 8;
+const WAGE_PER_HOURS = 20;
+
+/**  
+ * UC 2 Checking Employee Daily Wage
+*/
+let empHrs = 0;
+empCheck = Math.floor(Math.random() * 10) % 3;
+switch (empCheck) {
+    case IS_PART_TIME:
+        empHrs = PART_TIME_HOURS;
+        break;
+    case IS_FULL_TIME:
+        empHrs = FULL_TIME_HOURS;
+        break;
+    default:
+        empHrs = 0;    
+}
+
+let empWage = empHrs * WAGE_PER_HOURS;
+console.log("UC2- Emp Wage: " +empWage);
