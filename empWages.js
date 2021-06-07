3bd589abe65e8d6f26e5b6bb013a01e5335c659a
sanketb412 <sanketbagde38@gmail.com>
2021-06-07 16:30:42 +0530

    Added: code for storing Daily Wage in Array

--- empWages.js
+++ empWages.js
@@ -18,14 +18,25 @@ function getWorkingHours(empCheck) {
             return 0;
     }
 }
+function calcDailyWage(empHrs){
+    return empHrs * WAGE_PER_HOURS;
+}
 const NUM_OF_WORKING_DAYS = 20; 
 const MAX_HRS_IN_MONTH = 160;
 let totalEmpHrs = 0;
 let totalWorkingDays = 0;
+let empDailyWageArr = new Array();  //UC-6
+
 while (totalEmpHrs <= MAX_HRS_IN_MONTH && totalWorkingDays < NUM_OF_WORKING_DAYS){   /*UC 5: Added 'while loop' for getting 20 days and 160 hrs of wages.*/
     totalWorkingDays++
     let empCheck = Math.floor(Math.random() * 10) % 3;
-    totalEmpHrs += getWorkingHours(empCheck);
+    let empHrs = getWorkingHours(empCheck);
+    totalEmpHrs += empHrs;
+    console.log(empDailyWageArr.push(calcDailyWage(totalEmpHrs)));        //UC-6
+    
 }
-let empWage = totalEmpHrs * WAGE_PER_HOURS;
-console.log("UC5- Total Days: " + totalWorkingDays +" |Total Hours: "+ totalEmpHrs+" |Emp Wage: " +empWage);
+
+let empWage = calcDailyWage(totalEmpHrs);           // UC-6
+console.log("UC6- Total Days: " + totalWorkingDays +" |Total Hours: "+ totalEmpHrs+" |Emp Wage: " +empWage);
+
+
